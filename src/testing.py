acc2b4944aec6c25e4ccd4c733d2e3002ed1fec4
Alexsoil <alexsoilgr@gmail.com>
2026-01-01 23:03:54 +0200

    Initial model test case

--- src/testing.py
+++ src/testing.py
@@ -1,20 +1,25 @@
 from transformers import ViTImageProcessor, ViTForImageClassification
 from PIL import Image
+import os
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 image_path = 'akita_image.png'
 
 image = Image.open(image_path).convert('RGB')
-image.show()
+# image.show()
 
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+model_name = 'google/vit-base-patch16-224'
+
+processor = ViTImageProcessor.from_pretrained(model_name)
+model = ViTForImageClassification.from_pretrained(model_name)
 
 inputs = processor(images=image, return_tensors='pt')
 outputs = model(**inputs)
 
 logits = outputs.logits
+print(logits)
 predicted_class_idx = logits.argmax(-1).item()
 
 print("Predicted class:", model.config.id2label[predicted_class_idx])
-
-
