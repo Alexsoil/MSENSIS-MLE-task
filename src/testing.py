9bf62a3872b23d316c08008d80d57b777d438752
Alexsoil <alexsoilgr@gmail.com>
2026-01-06 20:27:50 +0200

    Updated to use fine tuned model from HuggingFace.

--- src/testing.py
+++ src/testing.py
@@ -1,6 +1,6 @@
 from Classifier import Classifier
 
 # classifier = Classifier()
-classifier = Classifier(model_name='finished_models/cats_dogs_finetune')
+classifier = Classifier(model_name='Alexsoil/ViT-cats-dogs')
 category, confidence= classifier.classify('cat_image.png')
 print(f'Image Class: {category} | Confidence: {confidence * 100:.2f}%')
